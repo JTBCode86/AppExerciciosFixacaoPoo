5ddc660ee96efe84e0ffcbbf96191ad25df7c258
Jean Barbosa <jeanbarbosa.it@gmail.com>
2021-11-14 20:40:46 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+
+namespace AppExerciciosFixacaoPoo
+{
+    /// <summary>
+    /// Exercicios - Fixação - POO - Classes
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo retangulo = new Retangulo();
+
+            Console.WriteLine("Entre a largura e altura do retângulo: ");
+            string[] vet = Console.ReadLine().Split(" ");
+
+            retangulo.Altura = double.Parse(vet[0]);
+            retangulo.Largura = double.Parse(vet[1]);
+            
+            double area = retangulo.Area();
+            double perimetro = retangulo.Perimetro();
+            double diagonal = retangulo.Diagonal();
+            
+            Console.Clear();
+            Console.WriteLine("AREA = {0}",area.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("PERÍMETRO = {0}",perimetro.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("DIAGONAL = {0}",diagonal.ToString("F2", CultureInfo.InvariantCulture));
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Retangulo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace AppExerciciosFixacaoPoo
+{
+    class Retangulo
+    {
+        public double Largura;
+        public double Altura;
+
+        public double Area() 
+        {
+            double area;
+            area = (Altura * Largura);
+            return area;
+        }
+
+        public double Perimetro() 
+        {
+            double perimetro;
+            perimetro = 2 * (Altura + Largura);
+            return perimetro;
+        
+        }
+
+        public double Diagonal()
+        {
+            double diagonal;
+            diagonal = Math.Sqrt(Math.Pow(Altura, 2) + Math.Pow(Largura, 2));
+            return diagonal;
+        }
+
+    }
+}
